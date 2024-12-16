04f4ce339a4153df320a20ad1026e0a04b0e6e1d
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2024-12-15 20:33:51 -0700

    fix

--- rust/src/day14/part2.rs
+++ rust/src/day14/part2.rs
@@ -12,7 +12,7 @@ use crate::{
 
 pub fn solve(input: &Input, x_size: i32, y_size: i32) -> Output {
     let mut answers = Vec::new();
-    let mut framebuffer = Framebuffer::new(1000);
+    let mut framebuffer = Framebuffer::new(10_000);
     for i in 0..10_000 {
         let mut end_points = HashMap::new();
         for robot in input {
